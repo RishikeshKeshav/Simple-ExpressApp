7aee31cc637f751caa6c11e4ff88f677098998af
RishikeshKeshav <rishikeshandhare1995@gmail.com>
2024-05-14 22:20:23 -0700

    commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Importing required modules
+const express = require('express');
+
+// Creating an instance of Express
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
